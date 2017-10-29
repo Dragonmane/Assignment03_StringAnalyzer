d06d65134eb0c33ca44c8614888f21f128ad6067
Ted Ginn <tginn@mail.greenriver.edu>
2017-10-29 13:39:25 -0700

    fixed three bugs
    Fixed 3 bugs

--- StringAnalyzer/app/src/main/java/co/miniforge/corey/stringanalyzer/AnalyzerActivity.java
+++ StringAnalyzer/app/src/main/java/co/miniforge/corey/stringanalyzer/AnalyzerActivity.java
@@ -1,7 +1,9 @@
 package co.miniforge.corey.stringanalyzer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -61,7 +63,15 @@ public class AnalyzerActivity extends AppCompatActivity {
 
         characterCount.setText(String.format("Character Count: %d", textUtil.getTextCharacterCount()));
         wordCount.setText(String.format("Word Count: %d", textUtil.getWordCount()));
+        uniqueWords.setText(String.format("Unique Words: %d", textUtil.getUniqueWords()));
+        longestWord.setText(String.format("Longest word: %d", textUtil.getLongestWord()));
         uniqueCharacters.setText(String.format("Unique Characters: %d", textUtil.getUniqueCharacters()));
         specialCharactersCount.setText(String.format("Special Characters: %d", textUtil.getSpecialCharacterCount()));
+    }
+
+    public void onClickListener(View view)
+    {
+        Intent intent = new Intent(AnalyzerActivity.this, InputActivity.class);
+        startActivity(intent);
     }
 }
--- StringAnalyzer/app/src/main/java/co/miniforge/corey/stringanalyzer/InputActivity.java
+++ StringAnalyzer/app/src/main/java/co/miniforge/corey/stringanalyzer/InputActivity.java
@@ -6,15 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class InputActivity extends AppCompatActivity {
+public class InputActivity extends AppCompatActivity
+{
     public static String intentTag = "inputData";
 
     EditText input;
     Button submit;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
 
@@ -23,18 +26,29 @@ public class InputActivity extends AppCompatActivity {
         bindFunctionality();
     }
 
-    public void locateViews(){
+    public void locateViews()
+    {
         input = (EditText) findViewById(R.id.stringToAnalyzeEditText);
         submit = (Button) findViewById(R.id.submitButton);
     }
 
-    public void bindFunctionality(){
-        submit.setOnClickListener(new View.OnClickListener() {
+    public void bindFunctionality()
+    {
+        submit.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), TextAnalyzerUtil.class);
-                intent.putExtra(intentTag, input.getText().toString());
-                startActivity(intent);
+            public void onClick(View view)
+            {
+                //inputMessage = input.getText().toString();
+                if (input.getText().toString().matches(""))
+                {
+                    Toast.makeText(InputActivity.this, "Please enter a message", Toast.LENGTH_SHORT).show();
+                } else
+                {
+                    Intent intent = new Intent(getApplicationContext(), AnalyzerActivity.class);
+                    intent.putExtra(intentTag, input.getText().toString());
+                    startActivity(intent);
+                }
             }
         });
     }
